f0648751007c37d4b97af725e8528321d5457e30
Saeed <saeed_et90@yahoo.com>
2021-09-21 16:17:11 +1000

    comments added + minor changes

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+//app config
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
 app.use(cors());
 
+//create server and socket config
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
@@ -15,26 +19,35 @@ const io = new Server(httpServer, {
   },
 });
 
+//app routes
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
+//socket events
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
+  // once connection has occurred emit socketId
+  socket.emit("socketid", socket.id);
 
+  // Register a handler for "calluser" event
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    // initialize the connection and send the caller info with an emit event
+    io.to(userToCall).emit("usercalling", { signal: signalData, from, name });
+  });
+
+  // Register a handler for "answercall" event
+  socket.on("answercall", (data) => {
+    // initialize the connection and send the connection data with an emit event
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+
+  // Register a handler for "disconnect" event
   socket.on("disconnect", () => {
     socket.broadcast.emit("callended");
   });
-
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
-  });
-
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("callaccepted", data.signal);
-  });
 });
 
+//app listenning
 httpServer.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
