5d92002c90c10c2eae34b048a33b66bc0f689cda
Saeed <saeed_et90@yahoo.com>
2021-09-21 11:07:13 +1000

    final server ready to deploy

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
